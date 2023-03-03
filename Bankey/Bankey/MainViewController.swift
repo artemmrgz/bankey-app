7158cbe3a08f6bf960df726ee3043b1338224962
Artem Marhaza <artemmrgz@gmail.com>
2023-03-03 14:18:41 +0100

    add tab bar

---
+++ Bankey/Bankey/MainViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MainViewController.swift
+//  Bankey
+//
+//  Created by Artem Marhaza on 03/03/2023.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupTabBar()
+    }
+    
+    private func setupViews() {
+        let summaryVC = AccountSummaryVewController()
+        let moneyVC = MoveMoneyViewController()
+        let moreVC = MoreViewController()
+        
+        summaryVC.setTabBarImage(imageName: "list.dash.header.rectangle", title: "Summary")
+        moneyVC.setTabBarImage(imageName: "arrow.left.arrow.right", title: "Move Money")
+        moreVC.setTabBarImage(imageName: "ellipsis.circle", title: "More")
+        
+        let summaryNC = UINavigationController(rootViewController: summaryVC)
+        let moneyNC = UINavigationController(rootViewController: moneyVC)
+        let moreNC = UINavigationController(rootViewController: moreVC)
+        
+        summaryNC.navigationBar.barTintColor = appColor
+        summaryNC.navigationBar.isTranslucent = false
+        
+        let tabBarList = [summaryNC, moneyNC, moreNC]
+        
+        viewControllers = tabBarList
+    }
+    
+    private func setupTabBar() {
+        tabBar.tintColor = appColor
+        tabBar.isTranslucent = false
+    }
+}
+
+class AccountSummaryVewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemRed
+    }
+}
+
+class MoveMoneyViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class MoreViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Artem Marhaza on 03/03/2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
