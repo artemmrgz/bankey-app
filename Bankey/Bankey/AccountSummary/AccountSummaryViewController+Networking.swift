c1eb38ff680019d1ad516acc9fee15e603445894
Artem Marhaza <artemmrgz@gmail.com>
2023-03-10 17:44:02 +0100

    add networking to AccountSummary

---
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController+Networking.swift
@@ -0,0 +1,85 @@
+//
+//  AccountSummaryViewController+Networking.swift
+//  Bankey
+//
+//  Created by Artem Marhaza on 10/03/2023.
+//
+
+import Foundation
+import UIKit
+
+enum NetworkError: Error {
+    case serverError
+    case decodingError
+}
+
+struct Profile: Codable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+}
+
+struct Account: Codable {
+    let id: String
+    let type: AccountType
+    let name: String
+    let amount: Decimal
+}
+
+extension AccountSummaryViewController {
+    func fetchProfile(forUserId userId: String, completion: @escaping (Result<Profile, NetworkError>) -> Void) {
+        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    completion(.failure(.serverError))
+                    return
+                }
+                
+                do {
+                    let profile = try JSONDecoder().decode(Profile.self, from: data)
+                    completion(.success(profile))
+                } catch {
+                    completion(.failure(.decodingError))
+                }
+            }
+        }.resume()
+    }
+}
+
+extension AccountSummaryViewController {
+    func fetchAccounts(forUserId userId: String, completion: @escaping (Result<[Account], NetworkError>) -> Void) {
+        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)/accounts")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    print("serverError")
+                    completion(.failure(.serverError))
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    
+                    let accounts = try decoder.decode([Account].self, from: data)
+                    print("success")
+                    completion(.success(accounts))
+                } catch {
+                    print("decodingError")
+                    completion(.failure(.decodingError))
+                }
+            }
+        }.resume()
+    }
+}
