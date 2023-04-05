e8b59a85704324050471a64ead2735db9aa1b716
Artem Marhaza <artemmrgz@gmail.com>
2023-04-05 14:37:55 +0200

    feat: create new password project

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Artem Marhaza on 05/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
